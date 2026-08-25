a2570b290e3314b58881ba9b4f9a9b6556bb1645
Klyntech <klyntyn09@gmail.com>
2026-08-25 19:25:08 +0100

    fix: use window.location.origin for API base so web login works on Render

--- web/js/config.js
+++ web/js/config.js
@@ -1,6 +1,7 @@
 window.NALLY = window.NALLY || {};
 
-NALLY.API = 'http://localhost:5000';
+// Same origin so login/API work on localhost and production (e.g. Render)
+NALLY.API = window.location.origin;
 NALLY.STORAGE_KEY = 'nally-token';
 NALLY.THEME_KEY = 'nally-theme';
 NALLY.COMPACT_KEY = 'nally-compact';
